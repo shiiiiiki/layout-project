156375d2e89e4cc1bba403b442ea1458067dd5d2
Tanya Pashkevich <shhhiki313@gmail.com>
2023-06-29 13:30:36 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+const APP = 'dist';
+let gulp = require('gulp');
+let sass = require('gulp-sass')(require('sass'));
+let cleanCSS = require('gulp-clean-css');
+let autoprefixer = require('gulp-autoprefixer');
+let rename = require('gulp-rename');
+let sourcemaps = require('gulp-sourcemaps');
+
+function buildStyles() {
+    return gulp.src(`./${APP}/scss/pages/*.scss`)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['node_modules'],
+        }).on('error', sass.logError))
+        .pipe(autoprefixer('last 3 versions'))
+        .pipe(
+            // Optional if you want to see not minified CSS file
+            gulp.dest(`./${APP}/styles`)
+        )
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write(`./`))
+        .pipe(gulp.dest(`./${APP}/styles`));
+}
+
+exports.buildStyles = buildStyles;
+exports.default = async function() {
+    gulp.watch(`./${APP}/scss/**/*.scss`, gulp.series(buildStyles));
+};
