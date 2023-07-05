ad712ec047523a743027b21d31191ba407976a36
Tanya Pashkevich <shhhiki313@gmail.com>
2023-07-05 15:17:09 +0300

    HW61. Gulp V5 - external packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,43 @@
 "use strict";
-const APP = "dist";
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const cleanCSS = require("gulp-clean-css");
-const autoprefixer = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
-const sourcemaps = require("gulp-sourcemaps");
+const gulp = require('gulp')
+const { src, dest, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
 
-function buildStyles() {
-  return gulp
-    .src(`./${APP}/scss/pages/*.scss`)
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({
-        includePaths: ["node_modules"],
-      }).on("error", sass.logError)
-    )
-    .pipe(autoprefixer("last 3 versions"))
-    .pipe(
-      // Optional if you want to see not minified CSS file
-      gulp.dest(`./${APP}/styles`)
-    )
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(sourcemaps.write(`./`))
-    .pipe(gulp.dest(`./${APP}/styles`));
-}
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/"
+  });
 
-exports.buildStyles = buildStyles;
-exports.default = async function () {
-  gulp.watch(`./${APP}/scss/**/*.scss`, gulp.series(buildStyles));
+  watch('build/sass/*.scss', buildSass);
+  watch('build/pages/*.pug', buildPug);
 };
 
+const buildSass = () => {
+  console.log('Компиляция SASS');
 
-const { series } = require('gulp');
-const { parallel, dest, watch } = require('gulp');
-
-function copyIndexScss() {
-  return gulp.src("dist/scss/index.scss").pipe(dest("build/styles/"));
+  return src('dist/sass/*.scss')
+      .pipe(sass())
+      .pipe(dest('build/styles/'))
+      .pipe(browserSync.stream());
 }
+
+const buildPug = () => {
+  console.log('Компиляция Pug');
+
+  return src('dist/pages/*.pug')
+      .pipe(pug())
+      .pipe(dest('build/'))
+      .pipe(browserSync.stream());
+}
+
+exports.server = browserSyncJob;
+exports.development = parallel(buildSass, buildPug);
 
 exports.default = async function () {
-  gulp.watch(`dist/scss/index.scss`);
+  gulp.watch('build/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('build/pug/**/*.pug', gulp.series('pug'));
+  gulp.watch('build/*.html').on('change', browserSync.reload);
 };
 
-function copyScss() {
-  return gulp
-    .src(["dist/scss/**/*.scss", "!dist/scss/components/**"])
-    .pipe(dest("build/styles/"));
-}
-
-exports.copy = parallel(copyIndexScss, copyScss);
-
-function sassCompile(done) {
-  console.log("Compile SASS to CSS");
-  console.log("Compile Pug to HTML");
-
-  done();
-}
-
-exports.layoutCompile = sassCompile;
-
-const changeAppStylesFile = (done) => {
-  console.log("Файл index.scss изменился");
-  done();
-};
-
-const checkFileStructure = (done) => {
-  console.log("Изменилась структура файлов");
-  done();
-};
-
-const watchers = () => {
-  watch("dist/scss/index.scss", { events: "change" }, changeAppStylesFile);
-  watch(
-    "dist/scss/components/",
-    { events: ["add", "unlink"] },
-    checkFileStructure
-  );
-};
-
-exports.watchers = watchers;
-exports.checkScss = series(copyIndexScss, copyScss, watchers);
-
-const jSChange = (done) => {
-  console.log(`File script.js was running tasks...`);
-  done();
-}
-function changingJSFiles() {
-  gulp.watch("dist/js/script.js", { events: 'change' }, jSChange);
-}
-
-exports.watchScriptJS = changingJSFiles;
